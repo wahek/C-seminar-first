92b3a2785020b08512b7c6d3de267c880d7a3c9d
wahek <wahek1999@mail.ru>
2022-09-17 19:59:06 +0300

    Второе задание

--- Program.cs
+++ Program.cs
@@ -4,21 +4,44 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+// Console.Write("Введите первое число: ");
+// int num0 = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int num1 = int.Parse(Console.ReadLine());
+// if (num1==num0){
+//     Console.Write("Числа равны");
+// }
+// else{
+//     if(num0 > num1){
+//     Console.Write("Первое число больше второго");
+//     }
+//     else{
+//         Console.Write("Второе число больше первого");
+//     }
+// }
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
 Console.Write("Введите первое число: ");
 int num0 = int.Parse(Console.ReadLine());
-
 Console.Write("Введите второе число: ");
 int num1 = int.Parse(Console.ReadLine());
-if (num1==num0){
-    Console.Write("Числа равны");
+Console.Write("Введите третее число: ");
+int num2 = int.Parse(Console.ReadLine());
+if(num0 > num1){
+    if (num0 > num2){
+        Console.Write("Первое число является максимальным");
+    }
+    else Console.Write("третее число является максимальным");
 }
 else{
-    if(num0 > num1){
-    Console.Write("Первое число больше второго");
+    if(num1 > num2){
+        Console.Write("Второе число является макисмальным");
     }
-    else{
-        Console.Write("Второе число больше первого");
-    }
+    else Console.Write("Третее число является максимальным");
 }
-
-
